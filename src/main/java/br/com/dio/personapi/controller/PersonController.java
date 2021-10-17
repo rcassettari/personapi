a7a1b94be742b44d8e052b75a2a1b44f63351f83
rcassettari <rcassettari@gmail.com>
2021-10-17 19:26:04 -0300

    First Commit for Person Api.

---
+++ src/main/java/br/com/dio/personapi/controller/PersonController.java
@@ -0,0 +1,16 @@
+package br.com.dio.personapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/people")
+public class PersonController {
+
+    @GetMapping
+    private String getBook()
+    {
+        return "API Test !";
+    }
+}
